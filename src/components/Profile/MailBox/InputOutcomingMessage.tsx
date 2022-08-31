0f85e244cdad3cf8c91688441fab0505ce5a9cb2
belmoz <sosnin.serg@gmail.com>
2022-08-31 22:39:25 +0300

    fix style mailbox

--- src/components/Profile/MailBox/InputOutcomingMessage.tsx
+++ src/components/Profile/MailBox/InputOutcomingMessage.tsx
@@ -1,6 +1,27 @@
 import { TextField } from "@mui/material";
 import { Container } from "@mui/system";
 import React, { useState } from "react";
+import styled from "styled-components";
+
+// const ContainerStyled = styled(Container)`
+const ContainerStyled = styled.div`
+  background: white;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  & textarea {
+    font-family: "Roboto","Helvetica","Arial",sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+      background: whitesmoke;
+      height: 100%;
+      width: 100%;
+      color: black;
+      resize: none;
+      line-height: 1.4;
+      padding: 6px 12px;
+  }
+`;
 
 const InputOutcomingMessage = () => {
   const [messageState, setMessageState] = useState({
@@ -15,11 +36,11 @@ const InputOutcomingMessage = () => {
     setMessageState((prev) => ({ ...prev, [name]: value }));
 
   return (
-    <Container style={{ background: "white", height: "100%" }}>
-      <TextField fullWidth name="recepient" label="enter recepient" variant="filled"  onChange={handleChange} />
+    <ContainerStyled>
+      <TextField fullWidth name="recepient" label="enter recepient" variant="filled" onChange={handleChange} />
       <TextField fullWidth name="subject" label="enter subject" variant="filled" onChange={handleChange} />
-      <input style={{background: "whitesmoke", height: "70%", width: "100%", color:"black"}} type="text" name="message"/>
-    </Container>
+      <textarea name="message" />
+    </ContainerStyled>
   );
 };
 
