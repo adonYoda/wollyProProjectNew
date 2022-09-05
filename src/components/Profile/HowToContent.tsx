c74bc00ab632f2b29a273652f249b02a043a7425
belmoz <sosnin.serg@gmail.com>
2022-09-05 11:41:05 +0300

    preFintish profilePage

--- src/components/Profile/HowToContent.tsx
+++ src/components/Profile/HowToContent.tsx
@@ -1,74 +1,174 @@
-import React from 'react'
-import styled from 'styled-components';
-import HowToPreview from './HowToPreview';
-import HowToStartPage from './HowToStartPage'
-
-
-const HowToContentWrapper = styled.div`
-	height: 240px;
-	background-color: #ffffff88;
-	overflow-x: scroll;
-	/* scrollbar-width: unset;
-	&::-webkit-scrollbar{
-		width: 0;height: 0;
-	} */
-`;
-const Container = styled.div`
-	padding: 20px 40px;
-	display: flex;
-`;
+import React from "react";
+import styled from "styled-components";
+import HowToPreview from "./HowToPreview";
+import HowToStartPage from "./HowToStartPage";
+import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
+import Slider from "react-slick";
+import { colors } from "../../utils/constants";
+import tempImg from "../../images/temp.jpeg";
 
 const contentPreview = [
 	{
-		path: 'get_started',
-		subtitle: 'how to start',
-		title: 'Get started together',
-		img: ''
+		path: "get_started",
+		subtitle: "How to start",
+		title: "Get started together",
+		img: tempImg,
 	},
 	{
-		path: 'present',
-		subtitle: 'your jewellry',
-		title: 'how to present',
-		img: ''
+		path: "present",
+		subtitle: "Your jewellry",
+		title: "How to present",
+		img: tempImg,
 	},
 	{
-		path: 'experience',
-		subtitle: 'provide & create',
-		title: 'customer experience',
-		img: ''
+		path: "experience",
+		subtitle: "Provide & create",
+		title: "Customer experience",
+		img: tempImg,
 	},
 	{
-		path: 'shipping',
-		subtitle: 'the quick guide',
-		title: 'to product shipping',
-		img: ''
+		path: "shipping",
+		subtitle: "The quick guide",
+		title: "To product shipping",
+		img: tempImg,
 	},
 	{
-		path: 'seller_fees',
-		subtitle: 'a guide to wolly pro seller and how to reduce',
-		title: 'seller fees',
-		img: ''
+		path: "seller_fees",
+		subtitle: "A guide to wolly pro seller and how to reduce",
+		title: "Seller fees",
+		img: tempImg,
 	},
-]
-
+];
+//=== <SLICK_SLIDER> ===========================================================================================
+const SliderStyled = styled(Slider)`
+	&.slick-slider {
+		/* margin: 30px auto 50px;
+		position: relative;
+		display: block;
+		user-select: none;
+		touch-action: pan-y;
+		-webkit-tap-highlight-color: transparent; */
+		max-width: 100%;
+		height: 240px;
+		display: block;
+		position: relative;
+		overflow: hidden;
+	}
+	& .slick-list,
+	.slick-slider {
+		/* position: relative;
+		display: block; */
+	}
+	& .slick-list {
+		padding: 20px;
+		height: 100%;
+	}
+	& .slick-track {
+		display: flex;
+		height: 100%;
+		/* position: relative;
+		top: 0;
+		left: 0;
+		display: block;
+		margin-left: auto; */
+	}
+	& .slick-slide {
+		max-width: 265px;
+		height: 155px;
+		margin-left: 20px;
+		transition: all 0.3s ease 0s;
+		&.slick-current {
+			max-width: 320px;
+			height: 100%;
+			& p {
+				transition: all 0.3s ease 0s;
+				font-size: 11px;
+			}
+			& h4 {
+				transition: all 0.3s ease 0s;
+				font-size: 24px;
+			}
+		}
+		& > div {
+			width: 100%;
+			height: 100%;
+		}
+	}
+	& .slick-arrow {
+		position: absolute;
+		top: 50%;
+		transform: translatey(-50%);
+		z-index: 20;
+		&.slick-prev {
+			left: 0;
+		}
+		&.slick-next {
+			right: 0;
+			transform: translatey(-50%) rotate(180deg);
+		}
+	}
+	& .slick-dots {
+		position: absolute;
+		bottom: 55px;
+		left: 75px;
+		& li {
+			display: inline-block;
+			margin: 0 25px 0 0;
+			&.slick-active {
+				& button {
+					background-color: ${colors.button.bg.main};
+				}
+			}
+			& button {
+				color: #fff;
+				border: 1px solid ${colors.button.border.main};
+				background-color: transparent;
+				width: 23px;
+				height: 23px;
+				font-size: 11px;
+				transition: all 0.3s ease 0s;
+				cursor: pointer;
+				&:hover {
+					border: 1px solid ${colors.button.border.hover};
+				}
+			}
+		}
+	}
+`;
+const settings = {
+	dots: true,
+	arrows: false,
+	infinite: false,
+	speed: 500,
+	slidesToShow: 3,
+	// slidesToScroll: 3,
+	swipeToSlide: false,
+	initialSlide: 0,
+	nextArrow: <ArrowBackIosRoundedIcon />,
+	prevArrow: <ArrowBackIosRoundedIcon />,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {},
+		},
+	],
+};
+//=== </SLICK_SLIDER> ===========================================================================================
 const HowToContent = () => {
-
-
-
 	return (
-		<HowToContentWrapper>
-			<Container>
-				{contentPreview.map((c, i) => <HowToPreview {...c} />)}
-				{/*
-				<HowToStartPage />
-				<HowToPresentPage />
-				<HowToProvideExperiencePage />
-				<HowToShipping />
-				<HowToReduceSellerFees />
-				*/}
-			</Container>
-		</HowToContentWrapper>
-	)
-}
+		<SliderStyled {...settings}>
+			{contentPreview.map((c, i) => (
+				<HowToPreview key={`${i}-${c}`} {...c} />
+			))}
+			{/*
+			<HowToStartPage />
+			<HowToPresentPage />
+			<HowToProvideExperiencePage />
+			<HowToShipping />
+			<HowToReduceSellerFees />
+			*/}
+		</SliderStyled>
+	);
+};
 
-export default HowToContent
+export default HowToContent;
--- src/components/Profile/HowToPreview.tsx
+++ src/components/Profile/HowToPreview.tsx
@@ -1,25 +1,81 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import { useNavigate } from "react-router";
+import styled from "styled-components";
 
 interface Props {
-    path: string,
-    subtitle: string,
-    title: string,
-    img: string
+	path: string;
+	subtitle: string;
+	title: string;
+	img: string;
 }
 
 const Wrapper = styled.div`
-    min-width: 267px;
-    background-color: lightgreen;
-    /* margin: 5px; */
+	height: 100%;
+	padding: 20px 100px 20px 30px;
+	position: relative;
+	cursor: pointer;
+	&:hover {
+		& .content__img {
+			&:after {
+				background-color: transparent;
+			}
+		}
+	}
 `;
-
+const Img = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	& img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		object-fit: cover;
+		width: 100%;
+		height: 100%;
+	}
+	&:after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		background-color: #0000006e;
+		width: 100%;
+		height: 100%;
+		transition: all 0.3s ease 0s;
+	}
+`;
+const Subtitle = styled.p`
+	text-transform: uppercase;
+	font-size: 8px;
+	padding-right: 50px;
+	color: #fff;
+	margin: 0 0 10px 0;
+	position: relative;
+`;
+const Title = styled.h4`
+	text-transform: none;
+	font-size: 20px;
+	line-height: 1.1;
+	color: #e7e7e7;
+	position: relative;
+`;
 const HowToPreview: React.FC<Props> = ({ path, subtitle, title, img }) => {
-    return (
-        <Wrapper>
-            <div>{subtitle} - {title}</div>
-        </Wrapper>
-    )
-}
+	const navigate = useNavigate();
 
-export default HowToPreview
+	return (
+		<Wrapper className="slick-content" onClick={() => navigate(path)}>
+			{img && (
+				<Img className="content__img">
+					<img src={img} alt={subtitle + " " + title} />
+				</Img>
+			)}
+			<Subtitle>{subtitle}</Subtitle>
+			<Title>{title}</Title>
+		</Wrapper>
+	);
+};
+
+export default HowToPreview;
